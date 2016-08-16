1fd72f78584403182340e4ac2e25042bad2827d3
Daniel Usani <daniel.e.usani@gmail.com>
2016-08-16 16:16:07 -0400

    Changed the file name for employee payroll exercise

--- week_1/day_2/employee_payroll.rb
+++ week_1/day_2/employee_payroll.rb
@@ -1,6 +1,3 @@
-# /---------------------------------------------------------
-#  ------------------------- SETUP -------------------------
-#  ---------------------------------------------------------/
 class Employee
     attr_reader :name, :email
     def initialize(name, email)
@@ -17,85 +14,82 @@ class HourlyEmployee < Employee
         @hours_worked = hours_worked
     end
 
-     def calculate_hourly_salary
-      #returns the hours worked * hourly_rate
-      @hourly_rate * @hours_worked
+    def calculate_salary
+      @hours_worked * @hourly_rate
     end
 end
 
 class SalariedEmployee < Employee
-    def initialize(name, email, salary_rate)
+    def initialize(name, email, yearly_salary)
         @name = name
         @email = email
-        @salary_rate = salary_rate
+        @yearly_salary = yearly_salary
     end
 
-    def calculate_salaried_salary
-    	@salary_rate / 52
+    def calculate_salary
+      @yearly_salary / 52
     end
 end
 
 class MultiPaymentEmployee < Employee
-    def initialize(name, email, salary_rate, hourly_rate, hours_worked)
+    def initialize(name, email,yearly_salary, hourly_rate, hours_worked)
         @name = name
         @email = email
-        @salary_rate = salary_rate
+        @yearly_salary = yearly_salary
         @hourly_rate = hourly_rate
         @hours_worked = hours_worked
     end
 
-   def calculate_multi_salary
-   		if @hours_worked > 40 then
-      	(((@hours_worked - 40) * (@hourly_rate)) + (@salary_rate)) / 52
+    def calculate_salary
+      if @hours_worked > 40 then
+      	(((@hours_worked - 40) * (@hourly_rate))+ (@yearly_salary)) / 52
       else
       	@yearly_salary / 52
       end
-   end
-    
+      
+    end
 end
 
 class Payroll
     def initialize(employees)
-      @employees = employees
-  end
-
-  def notify_employee(employee)
+        @employees = employees
+    end
+    
+     def notify_employee(x)
           #email them
-          puts "Hi #{name}, check your email: #{email} for details on your direct deposit amount"
-  end
+          puts "Notified #{x.name} via #{x.email} of amount deposited"
+  	end
 
-  def pay_employees
+	 def pay_employees
+       @employees.each do |x|
+        result = x.calculate_salary * 0.82
+        pretty_result = result.round(2)
+
+        puts "Paid #{x.name} $#{pretty_result} this week"
+      end
       # Should output how much we're paying each employee for this week and the total amount spent on payroll this week. 
-
-
-  end
+	 end
 end
 
-# /---------------------------------------------------------
-#  -------------------- APPLICATION CODE -------------------
-#  ---------------------------------------------------------/
-
-# josh = HourlyEmployee.new('Josh', 'nachoemail@example.com', 35, 50)
-
-# p josh.calculate_hourly_salary
-
-# puts ""
-
-# nizar = SalariedEmployee.new('Nizar', 'starcraftrulez@gmail.com', 1000000)
-
-# p nizar.calculate_salaried_salary
-
-# puts ""
-
-# ted = MultiPaymentEmployee.new('Ted', 'fortranr0x@gmail.com', 60000, 275, 55)
-
-# p ted.calculate_multi_salary
-
-      employees = [josh, nizar, ted, frank, daniel]
-	# payroll = Payroll.new(employees)
-	# payroll.pay_employees
-	employees(name)
+josh = HourlyEmployee.new('Josh', 'nachoemail@example.com', 35, 50)
+frank = HourlyEmployee.new('Frank','freakyfrank@ironhack.com', 15, 70)
+dan = HourlyEmployee.new('Dan','dancingdan@ironhack.com',15,70)
+nizar = SalariedEmployee.new('Nizar', 'starcraftrulez@gmail.com', 1000000)
+ted = MultiPaymentEmployee.new('Ted', 'fortranr0x@gmail.com', 60000, 275, 55)
 
 
+josh.calculate_salary
+ted.calculate_salary
+nizar.calculate_salary
 
+employees = [josh, frank, dan, nizar, ted]
 
+payroll = Payroll.new(employees)
+
+puts ""
+
+payroll.notify_employee(josh)
+
+puts ""
+
+payroll.pay_employees
