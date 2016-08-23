2c0812d08f12c7ecaadbcf3ae0f06e644bb1345b
Daniel Usani <daniel.e.usani@gmail.com>
2016-08-23 14:42:16 -0400

    Introduction to Sinatra

--- week_2/day_2/sinatra/app.rb
+++ week_2/day_2/sinatra/app.rb
@@ -37,3 +37,29 @@ get "/staff" do
 	erb :staff
 
 end
+
+users = [{username: "starwars", name: "Star Wars", bio: "The official home of Star Wars on Twitter", avatar: "https://pbs.twimg.com/profile_images/631535425333518336/D-i_GqpT.jpg"},
+		 {username: "ericartman", name: "Eric Cartman", bio: "The official home of Eric Cartman on Twitter", avatar: "https://pbs.twimg.com/profile_images/33686422/cartman.jpg"},
+		 {username: "daniel", name: "Daniel Usani", bio: "The official home of Daniel Usani on Iron Hack", avatar: "https://media.giphy.com/media/IGR4CXdHtqTjG/giphy.gif"}
+]
+
+# http://localhost:4567/users/ironhack
+# http://localhost:4567/users/starwars
+#                             --------
+#                                 |
+#                ------------------
+#                |
+get "/users/:username" do
+#                |
+#                --------------------
+#                                   |
+	@user_name_string = params[:username]
+
+	@the_user = users.find {|the_user| the_user[:username] == @user_name_string}
+
+	if @the_user == nil
+		erb :no_user
+	else
+	erb :user_profile
+	end
+end
