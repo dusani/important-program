99b9dbea8b02eb0b624093187fa71aa02091add3
Daniel Usani <daniel.e.usani@gmail.com>
2016-09-08 11:14:43 -0400

    Timetracking: Added a form to create a new project

--- week_4/timetracking/config/routes.rb
+++ week_4/timetracking/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get "/contact", to: 'site#contact'
   get "/say_name/:name", to: "site#say_name"
   get "/projects", to: "projects#index"
+  get "/projects/new", to: "projects#new"
   get "/projects/:id", to: "projects#show"
+  post "/projects", to: "projects#create"
 
 end
