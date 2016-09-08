54438c0f5b8bda2d5e02ae8701e4bf01b3047124
Daniel Usani <daniel.e.usani@gmail.com>
2016-09-08 09:56:28 -0400

    Added a detailed show page for a particular project

--- week_4/timetracking/app/controllers/projects_controller.rb
+++ week_4/timetracking/app/controllers/projects_controller.rb
@@ -3,4 +3,9 @@ class ProjectsController < ApplicationController
 		@projects = Project.order(created_at: :desc).limit(10)
 		render "index"
 	end
+
+	def show
+		#Display a single project from an id
+		@project = Project.find(params[:id])
+	end
 end
--- week_4/timetracking/config/routes.rb
+++ week_4/timetracking/config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "/contact", to: 'site#contact'
   get "/say_name/:name", to: "site#say_name"
   get "/projects", to: "projects#index"
+  get "/projects/:id", to: "projects#show"
 
 end
