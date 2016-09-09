139e72f43e6f6b7f47b1cd255629e1e8d84aa9ea
Daniel Usani <daniel.e.usani@gmail.com>
2016-09-09 17:38:41 -0400

    TimeTracking: Created links and forms for Prjoects and Time Entry

--- week_4/timetracking/app/controllers/time_entries_controller.rb
+++ week_4/timetracking/app/controllers/time_entries_controller.rb
@@ -1,4 +1,5 @@
 class TimeEntriesController < ApplicationController
+
 	def index
 		# Input
 		# - project id under params[:id]
@@ -24,13 +25,34 @@ class TimeEntriesController < ApplicationController
 		# 1. Find the project
 		# 2. Instantiate the new
 		project = Project.find(params[:project_id])
-		time_entry = project.time_entries.new(
-			minutes: params[:time_entry][:minutes],
-			hours: params[:time_entry][:hours],
-			date: params[:time_entry][:date],
-			)
+		time_entry = project.time_entries.new(time_entry_params)
 		time_entry.save
 
 		redirect_to "/projects/#{project.id}/time_entries"
+	end
+
+	def edit
+		# Input
+		# - params[:project_id]
+		# - params[:id]
+		# Output
+		# - The edit form
+		@project = Project.find(params[:project_id])
+		@time_entry = @project.time_entries.find(params[:id])
+	end
+
+	def update
+		project = Project.find(params[:project_id])
+		time_entry = project.time_entries.find(params[:id])
+
+		time_entry.update(time_entry_params)
+
+		redirect_to "/projects/#{project.id}/time_entries"
+	end
+
+	private
+
+	def time_entry_params
+		params.require(:time_entry).permit(:hours, :minutes, :date)
 	end
 end
--- week_4/timetracking/app/models/project.rb
+++ week_4/timetracking/app/models/project.rb
@@ -6,4 +6,8 @@ class Project < ApplicationRecord
 	validates :email, presence: true, uniqueness: true
 
 	validates :phone_number, length: { is: 10 }
+
+	def self.clean_old
+		where("created_at < ?", 7.days.ago).destroy_all
+	end
 end
