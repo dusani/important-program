15d705fa4e2bf9cf66beaa13458050f923fe0dbf
Daniel Usani <daniel.e.usani@gmail.com>
2016-09-12 12:23:12 -0400

    Concert Finder:Added lists of concerts to Concert page

--- week_4/day_5/concert_finder/app/controllers/concerts_controller.rb
+++ week_4/day_5/concert_finder/app/controllers/concerts_controller.rb
@@ -1,5 +1,15 @@
 class ConcertsController < ApplicationController
 
-	
+	def index
+		@concert = Concert.all
+	end
+
+	def new
+		@concert = Concert.new
+	end
+
+	def show
+		@concert = Concert.find(params[:id])
+	end
 
 end
