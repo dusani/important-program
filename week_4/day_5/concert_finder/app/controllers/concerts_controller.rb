bb208330816084f9d4f7853e333d782c82c9afbd
Daniel Usani <daniel.e.usani@gmail.com>
2016-09-12 15:13:18 -0400

    Concert Finder: Iteration 2

--- week_4/day_5/concert_finder/app/controllers/concerts_controller.rb
+++ week_4/day_5/concert_finder/app/controllers/concerts_controller.rb
@@ -8,8 +8,26 @@ class ConcertsController < ApplicationController
 		@concert = Concert.new
 	end
 
+	def create
+		concert = Concert.new(concerts_params)
+
+		if concert.save
+			redirect_to "/concerts"
+		else
+			@concert = concert
+
+			render "new"
+		end
+	end
+
 	def show
 		@concert = Concert.find(params[:id])
+	end
+
+	private
+
+	def concerts_params
+		params.require(:concert).permit(:name, :artist, :venue, :city, :date, :price, :description)
 	end
 
 end
--- week_4/day_5/concert_finder/app/controllers/site_controller.rb
+++ week_4/day_5/concert_finder/app/controllers/site_controller.rb
@@ -1,5 +1,8 @@
 class SiteController < ApplicationController
 
 	def home
+		@current_concerts = Concert.where(date: DateTime.now..DateTime.now.end_of_day)
+		@month_concerts = Concert.where(date: DateTime.now.end_of_day + 1.day..DateTime.now.end_of_month)
 	end
+
 end
