0d2e9901f2efb8ef2812440c362c929a40e8667a
Daniel Usani <daniel.e.usani@gmail.com>
2016-09-12 10:20:48 -0400

    TimeTracking: Simplified the resources for routes

--- week_4/timetracking/config/routes.rb
+++ week_4/timetracking/config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # get ('/', {to: 'site#home'})
   get "/contact", to: 'site#contact'
 
-  resources :projects do
-    resources :time_entries
+  resources :projects, only: [:index, :show, :new, :create] do
+    resources :time_entries, except: [:show]
   end
 
     # get "/projects", to: "projects#index"
