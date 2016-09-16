ecc12dac64dab69dceb8d0dc5a8c50e3f29f26c6
Daniel Usani <daniel.e.usani@gmail.com>
2016-09-16 11:25:06 -0400

    Spotify Player: Iteration 1 - Song Search

---
+++ week_5/day_5/spotify_player/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function (){
+	console.log("Document Ready")
+	$(".js-search").on("click", fetchSongs)
+
+	function fetchSongs (theEvent){
+		theEvent.preventDefault();
+
+		console.log("Fetch Songs on Click");
+
+		var song = $(".js-song-name").val();
+
+		$.ajax({
+			type: "GET",
+			url: `https://api.spotify.com/v1/search?type=track&q=${song}`,
+			success: showSongs,
+			error: handleError,
+		});
+	};
+
+	function showSongs (response){
+
+		console.log("Success");
+		console.log(response);
+
+		var firstSong = response.tracks.items[0];
+
+		console.log(firstSong);
+
+		$(".title").html(firstSong.name);
+		$(".author").html(firstSong.artists[0].name);
+		document.getElementById("js-album-image").src = (firstSong.album.images[0].url);
+
+	};
+
+	
+	function handleError (error) {
+		console.log("Error!");
+		console.log(error.responseText);
+	};
+});
