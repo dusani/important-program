463e9f2d757f01d0e9defaef7b701009ca49cfbb
Daniel Usani <daniel.e.usani@gmail.com>
2016-09-21 16:23:02 -0400

    Sandwicher Advance

---
+++ week_6/day_3/Sandwichr/app/controllers/sandwich_views_controller.rb
@@ -0,0 +1,23 @@
+class SandwichViewsController < ApplicationController
+		before_action :authenticate_user!, only: [:index, :show]
+
+	def index
+		@sandwich = Sandwich.all
+	end
+
+	def show
+		@sandwich = Sandwich.find_by(id: params[:id])
+		@ingredients = @sandwich.ingredients	
+		@all_ingredients = Ingredient.all 
+
+		# _id = SandwichIngredient.find_by(ingredient_id: params[:ingredient_id])
+	end
+
+	def new
+
+	end
+
+	def create
+
+	end
+end
